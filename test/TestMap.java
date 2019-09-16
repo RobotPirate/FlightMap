2b6c73242461577bbd1722c5541aac9c2a6ecd75
Cerina da Graca <dagraca@usc.edu>
2019-09-15 20:45:45 -0700

    build.xml built out to cover test
    made test targets in build.xml
    Test runs jUnit tests, compiled sample jUnit test.

---
+++ test/TestMap.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Project1.SearchMap;
+
+public class TestMap {
+  @Test
+  public void evaluatesExpression() {
+   SearchMap s = new SearchMap();
+//    int sum = calculator.evaluate("1+2+3");
+//    assertEquals(6, sum);
+   System.out.println("TESTTESTTEST");
+  }
+}
