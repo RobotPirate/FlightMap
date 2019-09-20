181651206c748bdb563e159e47604ad25dd1de70
Cerina da Graca <dagraca@usc.edu>
2019-09-19 21:39:31 -0700

    Updated gitignore
    Updated gitIgnore to ignore the output.txt file. Also made the main SearchMap function take in arguments instead of hardcoding the input and output files in

--- src/SearchMap.java
+++ src/SearchMap.java
@@ -4,30 +4,18 @@ import java.io.IOException;
 public class SearchMap {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-//		String inputFile = args[0];
-//		String outputFile = args[1];
+
+		String inputFile = args[0];
+		String outputFile = args[1];
 		
-		FlightMap fm = new FlightMap("input.txt", "output.txt");
-//		System.out.println("In SearchMap");
+		FlightMap fm = new FlightMap(inputFile, outputFile);
+
 		try{
 			fm.getFlightCosts();
 		}
 		catch(IOException e){
 			System.out.println("IOException: " + e.getMessage());
 		}
-		
-		//returns the outputFile string
-		
-				
-		
-//		File myFile = new File("input.txt");
-//        System.out.println("Attempting to read from file in: "+myFile.getCanonicalPath());
-		
-		
-		
-		
-		
 	}
 
 }
